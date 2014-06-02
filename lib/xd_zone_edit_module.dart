727a15d7c7f91734b01e2c173fece3cd24c207c1
Megan Sharon <me@megansharon.com>
2014-06-02 03:51:34 -0400

    create dns service, add load project and autologin

--- lib/xd_zone_edit_module.dart
+++ lib/xd_zone_edit_module.dart
@@ -3,10 +3,12 @@ library xdZoneEdit.xd_zone_edit_module;
 import 'package:angular/angular.dart';
 import 'package:xdZoneEdit/xd_zone_edit_controller.dart';
 import 'package:xdZoneEdit/oauth/google_oauth2_service.dart';
+import 'package:xdZoneEdit/dns/google_cloud_dns.dart';
 
 class XDZoneEditModule extends Module {
   XDZoneEditModule() {
     type(GoogleOauth2Service);
+    type(GoogleCloudDns);
     type(XDZoneEditController);
   }
 }
