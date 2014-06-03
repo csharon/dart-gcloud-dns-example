8286fe01dd414f132bdd391f91fe746e5bd42250
Chris Sharon <chris@xdoji.com>
2014-06-03 17:34:03 -0400

    created appHeader component

--- web/main.dart
+++ web/main.dart
@@ -1,9 +1,9 @@
 library xdZoneEdit.main;
 
 import 'package:angular/angular.dart';
-//import 'package:angular/application_factory.dart';
 import 'package:xdZoneEdit/xd_zone_edit_module.dart';
-//import 'package:xdZoneEdit/oauth/google_oauth2_module.dart';
+
+
 @MirrorsUsed(override: '*')
 import 'dart:mirrors';
 
