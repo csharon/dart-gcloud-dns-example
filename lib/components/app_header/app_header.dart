f1affe401c530423e174493c23168fef999ec46b
Megan Sharon <me@megansharon.com>
2014-06-10 03:51:10 -0400

    added oauth2 v2 lib, get userinfo, display picture and logout dropdown

--- lib/components/app_header/app_header.dart
+++ lib/components/app_header/app_header.dart
@@ -2,21 +2,26 @@ library xdZoneEdit.components.app_header.app_header;
 
 import 'package:angular/angular.dart';
 import 'package:xdZoneEdit/oauth/google_oauth2_service.dart';
+import 'package:bootjack/bootjack.dart';
 
 @Component(
   selector: 'app-header',
   templateUrl: 'packages/xdZoneEdit/components/app_header/app_header.html',
   cssUrl: 'packages/xdZoneEdit/components/app_header/app_header.css',
-  publishAs: 'appHeader'
+  publishAs: 'appHeader',
+  useShadowDom: false
 )
 class AppHeader {
 
   GoogleOauth2Service gas;
   String appName = 'xdZoneEdit';
 
-  AppHeader(this.gas);
+  AppHeader(this.gas){
+    Dropdown.use();
+  }
 
   bool get isAuthenticated => gas.isAuthenticated;
+  String get userPic => gas.profile != null ? gas.profile.picture : '';
 
   void login() {
     gas.login();
--- lib/oauth/google_oauth2_service.dart
+++ lib/oauth/google_oauth2_service.dart
@@ -2,7 +2,8 @@ library xdZoneEdit.oauth.google_oauth2_service;
 
 import 'package:angular/angular.dart';
 import 'dart:async' show Future;
-import "package:google_oauth2_client/google_oauth2_browser.dart";
+import 'package:google_oauth2_v2_api/oauth2_v2_api_browser.dart' as oauth2client;
+import 'package:google_oauth2_client/google_oauth2_browser.dart';
 import 'package:google_dns_v1beta1_api/dns_v1beta1_api_browser.dart';
 import 'package:google_dns_v1beta1_api/dns_v1beta1_api_client.dart';
 
@@ -12,19 +13,36 @@ class GoogleOauth2Service {
 
   static final String clientWeb = '795904425230-d777qbrp5vsbbk9jsqegd4ne115vmtgm.apps.googleusercontent.com';
   static final List<String> scopes = [
+      oauth2client.Oauth2.USERINFO_EMAIL_SCOPE,
+      oauth2client.Oauth2.USERINFO_PROFILE_SCOPE,
       'openid',
-      'email',
-      'profile',
       Dns.NDEV_CLOUDDNS_READWRITE_SCOPE,
       Dns.CLOUD_PLATFORM_SCOPE
   ];
 
-  final auth = new GoogleOAuth2(clientWeb, scopes, autoLogin: true);
+  var _profile;
 
-  GoogleOauth2Service();
+  GoogleOAuth2 _auth;
+  var _oauth2;
+
+  GoogleOauth2Service() {
+    _auth = new GoogleOAuth2(clientWeb, scopes, autoLogin: true, tokenLoaded: onTokenLoaded);
+    _oauth2 = new oauth2client.Oauth2(_auth);
+  }
+
+  void onTokenLoaded(Token token) {
+
+    _oauth2.oauth_token = token.data;
+    _oauth2.userinfo.get().then(
+        (resp) {
+          _profile = resp;
+        }
+    ).catchError((error) => print(error));
+  }
 
 
   Future login(){
+
     return auth.login();
   }
 
@@ -35,5 +53,9 @@ class GoogleOauth2Service {
   bool get isAuthenticated {
     return auth.token != null;
   }
+
+  GoogleOAuth2 get auth => _auth;
+  Token get accessToken => _auth.token.data;
+  Object get profile => _profile;
 
 }
