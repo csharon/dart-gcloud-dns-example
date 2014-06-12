e8401bd0a3324d00ba61aa071e6f3c4283c79569
Chris Sharon <chris@xdoji.com>
2014-06-12 18:12:38 -0400

    update project name

--- lib/xd_zone_edit_module.dart
+++ lib/xd_zone_edit_module.dart
@@ -1,15 +1,15 @@
-library xdZoneEdit.xd_zone_edit_module;
+library dns_editor.xd_zone_edit_module;
 
 import 'package:angular/angular.dart';
-import 'package:xdZoneEdit/oauth/google_oauth2_service.dart';
-import 'package:xdZoneEdit/dns/google_cloud_dns.dart';
-import 'package:xdZoneEdit/local_storage/local_storage_service.dart';
-import 'package:xdZoneEdit/dns/project_manager.dart';
-import 'package:xdZoneEdit/dns/zone_manager.dart';
-import 'package:xdZoneEdit/components/app_header/app_header.dart';
-import 'package:xdZoneEdit/components/zone_selector/zone_selector.dart';
-import 'package:xdZoneEdit/components/zone_form/zone_form.dart';
-import 'package:xdZoneEdit/components/zone_record_list/zone_record_list.dart';
+import 'package:dns_editor/oauth/google_oauth2_service.dart';
+import 'package:dns_editor/dns/google_cloud_dns.dart';
+import 'package:dns_editor/local_storage/local_storage_service.dart';
+import 'package:dns_editor/dns/project_manager.dart';
+import 'package:dns_editor/dns/zone_manager.dart';
+import 'package:dns_editor/components/app_header/app_header.dart';
+import 'package:dns_editor/components/zone_selector/zone_selector.dart';
+import 'package:dns_editor/components/zone_form/zone_form.dart';
+import 'package:dns_editor/components/zone_record_list/zone_record_list.dart';
 
 class XDZoneEditModule extends Module {
   XDZoneEditModule() {
